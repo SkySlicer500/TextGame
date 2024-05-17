16c04def9b83e74a58ad7da38dcf94ccff67963e
SkySlicer500 <166448149+SkySlicer500@users.noreply.github.com>
2024-05-17 19:32:02 -0400

    Update textGame.py
    Refactored to hold main functioning mechanics

--- textGame.py
+++ textGame.py
@@ -1,1 +1,16 @@
-print("Hello World")
+def start():
+  return(0)
+
+def update():
+  return(0)
+
+def end(errorCode):
+  print("Error:", errorCode)
+
+def main():
+  num = start()
+  while(num == 0):
+    num = update()
+  end(num)
+
+main()

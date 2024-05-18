0349714a4218c85cce02173950ab7e3bc010a10a
SkySlicer500 <166448149+SkySlicer500@users.noreply.github.com>
2024-05-18 12:34:05 -0400

    Update textGame.py

--- textGame.py
+++ textGame.py
@@ -6,13 +6,16 @@ currentArea = 0
 def interact():
   playerIn = input().split()
   #Keywords for actions bellow
-  if (playerIn[0] == "here"):
+  if (playerIn[0] == "help"):
+    print("here - tells you about the current area")
+  elif (playerIn[0] == "here"):
     print(currentArea["area"][1]) #Give information of the current location
   else:
-    print("You were too lost in thought to do anything")
+    print("You were too lost in thought to do anything, you consider looking for \"help\" to remember what you were doing.")
 
 def start():
   global currentArea
+  print("\nWelcome to Text Game, type \"help\" for usable commands.\n")
   file = open("area1.json")
   currentArea = json.load(file)
   print("You have entered:", currentArea["area"][0])

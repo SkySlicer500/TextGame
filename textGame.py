bf586a523d33740e7c8f26fcbcd1e9d34cb8d0c4
SkySlicer500 <166448149+SkySlicer500@users.noreply.github.com>
2024-05-18 14:06:36 -0400

    Update textGame.py
    Basic commands have been added, and json compatibility should be fully implemented.

--- textGame.py
+++ textGame.py
@@ -2,36 +2,76 @@ import json
 
 inventory = []
 currentArea = 0
+currentRoom = ""
 
 def interact():
-  playerIn = input().split()
-  #Keywords for actions bellow
-  if (playerIn[0] == "help"):
-    print("here - tells you about the current area")
-  elif (playerIn[0] == "here"):
-    print(currentArea["area"][1]) #Give information of the current location
-  else:
-    print("You were too lost in thought to do anything, you consider looking for \"help\" to remember what you were doing.")
+    global currentRoom
+    playerIn = input().split()
+    try:
+        #Keywords for actions bellow
+        keyword = playerIn[0]
+        if (keyword == "help"):
+            print("here - observe your current surroundings\ngoto - go to a different room\ngrab - pickup an item that you find in a room\ntalkto - have a conversation with someone in a room\ninv - opens the inventory")
+        elif (keyword == "here"):
+            print(currentArea[currentRoom]["room"][0])
+        elif (keyword == "goto"):
+            try:
+                location = playerIn[1]
+                for x in range(len(currentArea[currentRoom]["room"][1])):
+                    if (currentArea[currentRoom]["room"][1][x] == location):
+                        currentRoom = currentArea[currentRoom]["room"][1][x]
+                        break
+                print("You went to", currentRoom)
+            except(Exception):
+                print("Go to where?")
+        elif (keyword == "grab"):
+            try:
+                item = playerIn[1]
+                for x in range(len(currentArea[currentRoom]["items"])):
+                    if (currentArea[currentRoom]["items"][x][0] == item):
+                        inventory.append(currentArea[currentRoom]["items"][x])
+                        print(currentArea[currentRoom]["items"][x][0], "was added to your inventory!")
+                        break
+            except(Exception):
+                print("Grab what?")
+        elif (keyword == "talkto"):
+            try:
+                person = playerIn[1]
+                for x in range(len(currentArea[currentRoom]["characters"])):
+                    if (currentArea[currentRoom]["characters"][x][0] == person):
+                        print(currentArea[currentRoom]["characters"][x][0], ":", currentArea[currentRoom]["characters"][x][2])
+                        break
+            except(Exception):
+                print("Talk to who?")
+        elif (keyword == "inv"):
+            print(inventory)
+        else:
+            print("You were too lost in thought to do anything, you consider looking for \"help\" to remember what you were doing.")
+    except(Exception):
+        print("You were too lost in thought to do anything, you consider looking for \"help\" to remember what you were doing.")
+
 
 def start():
-  global currentArea
-  print("\nWelcome to Text Game, type \"help\" for usable commands.\n")
-  file = open("area1.json")
-  currentArea = json.load(file)
-  print("You have entered:", currentArea["area"][0])
-  return(0)
+    global currentArea, currentRoom
+    print("\nWelcome to Text Game, type \"help\" for usable commands.\n")
+    file = open("area1.json")
+    currentArea = json.load(file)
+    currentRoom = currentArea["area"][2]
+    print("You have entered:", currentArea["area"][0])
+    print(currentArea[currentRoom]["room"][0])
+    return(0)
 
 def update():
-  interact()
-  return(0)
+    interact()
+    return(0)
 
 def end(errorCode):
-  print("Error:", errorCode)
+    print("Error:", errorCode)
 
 def main():
-  num = start()
-  while(num == 0):
-    num = update()
-  end(num)
+    num = start()
+    while(num == 0):
+        num = update()
+    end(num)
 
 main()

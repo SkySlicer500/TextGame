2e050e15345cab928527ea58cbd31f35ce1b5fa3
SkySlicer500 <166448149+SkySlicer500@users.noreply.github.com>
2024-05-17 20:07:45 -0400

    Update textGame.py
    Set up the item system for later use

--- textGame.py
+++ textGame.py
@@ -1,3 +1,12 @@
+area = 0
+
+class Item:
+  def __innit__(self, name, type, stat, equipability):
+    self.name = name #The name of the created item
+    self.type = type #Whether the item is a weapon, piece of armor, food etc.
+    self.stat = stat #In relation to the item type, how effective it is, ex: weapon stat would be damage, food stat would be saturation
+    self.equipability = equipability #0 if it can't be equipped, otherwise to designate specific armor or accessory slot
+
 def start():
   return(0)
 

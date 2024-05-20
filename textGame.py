5642bb3f9b64b595e7d21c80cbedac210dfe6d1b
SkySlicer500 <166448149+SkySlicer500@users.noreply.github.com>
2024-05-19 20:09:09 -0400

    Update textGame.py
    Even more event system updates

--- textGame.py
+++ textGame.py
@@ -5,7 +5,7 @@ currentArea = 0
 currentRoom = ""
 
 def interact():
-    global currentArea, currentRoom
+    global currentArea, currentRoom, inventory
     print()
     playerIn = input().split()
     print()
@@ -59,6 +59,11 @@ def interact():
                 for x in range(len(currentArea[currentRoom]["characters"])):
                     if (currentArea[currentRoom]["characters"][x][0] == person):
                         print(currentArea[currentRoom]["characters"][x][0], ":", currentArea[currentRoom]["characters"][x][2])
+                        try:
+                            if (currentArea[currentRoom]["characters"][x][3] != None):
+                                triggerEvent(currentArea[currentRoom]["characters"][x][3])
+                        except(Exception):
+                            pass
                         break
                     if (x == len(currentArea[currentRoom]["characters"])-1):
                         print("You searched, but were unable to find anyone by that name.")
@@ -66,43 +71,67 @@ def interact():
                 print("Talk to who?")
         elif (keyword == "inv"):
             print(inventory)
+        elif (keyword == "inspect"):
+            try:
+                structure = playerIn[1]
+                for x in range(len(currentArea[currentRoom]["structures"])):
+                    if (currentArea[currentRoom]["structures"][x][0] == structure):
+                        print(currentArea[currentRoom]["structures"][x][1])
+                        break
+                    if (x == len(currentArea[currentRoom]["structures"])-1):
+                        print("You searched, but were unable to find anything by that name.")
+            except(Exception):
+                print("Inspect what?")
         elif (keyword == "use"):
             try:
                 if (playerIn[2] == "on"):
-                    item = playerIn[1]
-                    event = playerIn[3]
-                    for x in range(len(inventory)):
-                        if (inventory[x][0] == item):
-                            for y in range(len(currentArea[currentRoom]["events"])):
-                                if (currentArea[currentRoom]["events"][y][0] == event):
-                                    if (inventory[x][2] == currentArea[currentRoom]["events"][y][1]):
-                                        print(currentArea[currentRoom]["events"][y][2])
-                                        type = currentArea[currentRoom]["events"][y][3]
-                                        effect = currentArea[currentRoom]["events"][y][4]
-                                        if (type == "room"):
-                                            currentArea[currentRoom]["room"][1].append(effect)
-                                        elif (type == "characters"):
-                                            currentArea[currentRoom]["characters"].append(effect)
-                                        elif (type == "items"):
-                                            currentArea[currentRoom]["items"].append(effect)
-                                        elif (type == "enemies"):
-                                            currentArea[currentRoom]["enemies"].append(effect)
-                                        elif (type == "events"):
-                                            currentArea[currentRoom]["enemies"].append(effect)
-                                    break
-                                if (y == len(currentArea[currentRoom]["events"])-1):
-                                    print("Such a thing was not found in the room.")
-                            break
-                        if (x == len(inventory)-1):
-                            print("You couldn't find an item like that in your inventory.")
                     if (0 == len(inventory)):
                         print("You couldn't find any items in your inventory.")
+                    else:
+                        item = playerIn[1]
+                        event = playerIn[3]
+                        for x in range(len(inventory)):
+                            if (inventory[x][0] == item):
+                                triggerEvent(inventory[x][2])
+                                break
+                            if (x == len(inventory)-1):
+                                print("You couldn't find an item like that in your inventory.")
             except(Exception):
                 print("Use what on what? That didn't seem to work...")         
         else:
             print("You were too lost in thought to do anything, you consider looking for \"help\" to remember what you were doing.")
     except(Exception):
         print("You were too lost in thought to do anything, you consider looking for \"help\" to remember what you were doing.")
+
+def triggerEvent(eventKey):
+    global currentArea, currentRoom, inventory
+    for x in range(len(currentArea[currentRoom]["events"])):
+        if (eventKey == currentArea[currentRoom]["events"][x][1]):
+            print(currentArea[currentRoom]["events"][x][2])
+            for y in range(len(currentArea[currentRoom]["events"][x][3])):
+                action = currentArea[currentRoom]["events"][x][3][y][0]
+                key = currentArea[currentRoom]["events"][x][3][y][1]
+                effect = currentArea[currentRoom]["events"][x][3][y][2]
+                if (action == "add"):
+                    if (key == "room"):
+                        currentArea[currentRoom]["room"][1].append(effect)
+                    elif (key == "inventory"):
+                        inventory.append(effect)
+                    else:
+                        currentArea[currentRoom][key].append(effect)
+                elif (action == "del"):
+                    if (key == "room"):
+                        for z in range(len(currentArea[currentRoom]["room"][1])):
+                            if (currentArea[currentRoom]["room"][1][z] == effect):
+                                del(currentArea[currentRoom]["room"][1][z])
+                    elif (key == "inventory"):
+                        for z in range(len(inventory)):
+                            if (inventory[z][0] == effect):
+                                del(inventory[z])
+                    else:
+                        for z in range(len(currentArea[currentRoom][key])):
+                            if (currentArea[currentRoom][key][z][0] == effect):
+                                del(currentArea[currentRoom][key][z])
 
 def start():
     global currentArea, currentRoom
